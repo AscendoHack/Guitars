351459f4e7edeaeca4bca288549a11f2ebad71e7
KEMPRUT <52598832+AscendoHack@users.noreply.github.com>
2019-08-20 09:36:45 +0700

    Add files via upload

---
+++ 200819.sh
@@ -0,0 +1,289 @@
+#!/bin/bash
+# yang record gw doain ilmu lu ga manfaat
+# aubscribe chanel youtube ENTUP ID
+read -p "MASUKAN NAMA ANDA : " nama
+clear
+toilet -f standard -F gay INSTALLER V2
+screenfetch -A Linux
+echo "╔═════════════════════════════════════════╗" | lolcat
+echo "║ Author  : TU4N3NTUP                     ║" | lolcat
+echo "║ Youtube : ENTUP ID                      ║" | lolcat
+echo "║ github  : https://github.com/KEMPRUT    ║" | lolcat
+echo "║ Facebook: ENTUP ID                      ║" | lolcat
+echo "╚═════════════════════════════════════════╝" | lolcat
+echo
+echo "╔══════════════════════════════════════════" | lolcat
+echo "║ [√] Nama :" $nama
+echo "╚══════════════════════════════════════════" | lolcat
+echo
+echo "═══════════════════════════════════════════
+[ 01 ] Bruteforce
+[ 02 ] InstaZedd
+[ 03 ] Hack Target
+[ 04 ] Crack Fb
+[ 05 ] Fake Fb
+[ 06 ] Hack Cctv
+[ 07 ] Yahoo Clone
+[ 08 ] Auto Mbf
+[ 09 ] Sadap Camera
+[ 10 ] ROOM
+[ 11 ] MARSHALL
+[ 12 ] Dark Mod2
+[ 13 ] Dark 4
+[ 14 ] Vip-1.9
+[ 15 ] Dark Target
+[ 16 ] Hack Fbi
+[ 17 ] Hack Fb masal
+[ 18 ] dark v1.6
+[ 19 ] dark v1.7
+[ 20 ] dark PREMIUM
+[ 21 ] Dark vvip
+[ 22 ] KELUAR
+═══════════════════════════════════════════" | lolcat
+echo
+read -p " root@localcrot ==》 " nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+clear
+pkg install python2
+pkg install git
+git clone https://github.com/rezadkim/instazedd
+pip2 install bs4
+pip2 install mechanize
+pip2 install requests
+cd instazedd
+python2 igz.py
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+clear
+pkg install git
+git clone https://github.com/DarkCurut08/DarkCurut08
+ls
+cd DarkCurut08
+ls
+sh Moreno77.sh
+fi
+
+if [ $nomor = 4 ] || [ $nomor = 04 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/crackfb
+cd crackfb
+python2 crack.py
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+clear
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/EgiRecod/FakeFB
+cd FakeFB
+python2 fake.py
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+pkg install python2
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+clear
+apt update && apt upgrade
+apt install git
+apt install python2
+apt install figlet
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then
+clear
+pkg update && pkg upgrdae -y
+pkg install python2 -y
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/unikers71/autombf
+cd autombf
+python2 mbf.py
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 09 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $nomor = 10 ] || [ $nomor = 10 ]
+then
+clear
+pkg install python2
+pkg install curl
+pkg install git
+git clone https://github.com/rezadkim/dark-room
+cd dark-room
+python2 chat.py
+fi
+
+if [ $nomor = 11 ] || [ $nomor = 11 ]
+then
+clear
+git clone https://github.com/Rusmana-ID/encript-python-marshall 
+cd encript-python-marshall 
+sh encript-python.sh
+fi
+
+if [ $nomor = 12 ] || [ $nomor = 12 ]
+then
+clear
+pkg upgrade && pkg update 
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/cyber2000
+ls
+cd cyber2000
+ls
+python2 Dark2.py
+fi
+
+if [ $nomor = 13 ] || [ $nomor = 13 ]
+then
+clear
+ pkg update
+pkg upgrade
+pkg install python2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/cyber2611/Dark4
+ls
+cd Dark4
+ls
+python2 Dark4.py
+fi
+
+if [ $nomor = 14 ] || [ $nomor = 14 ]
+then
+clear
+pkg install python2
+pip2 install mechanize
+pip2 install requests
+pkg install git 
+git clone https://github.com/cyber2611/BHV26
+ls
+cd BHV26
+ls
+python2 vip-1.9.py
+fi
+
+if [ $nomor = 15 ] || [ $nomor = 15 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/gellmoxer/gm13
+cd gm13
+python2 gm13.py
+fi
+
+if [ $nomor = 16 ] || [ $nomor = 16 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/gellmoxer/FBI
+cd FBI
+python2 fbi.py
+fi
+
+if [ $nomor = 17 ] || [ $nomor = 17 ]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pip2 install mechanize
+pkg install git
+git clone https://github.com/gellmoxer/reggae
+cd reggae
+python2 reggae.py
+fi
+
+if [ $nomor = 18 ] || [ $nomor = 18 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+pip2 install requests
+pip2 install mechanize
+python2 SETAN.py
+fi
+
+if [ $nomor = 19 ] || [ $nomor = 19 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/fb1.7
+cd fb1.7
+pip2 install requests mechanize
+python2 dark.py
+fi
+
+if [ $nomor = 20 ] || [ $nomor = 20 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+pip2 install -r requirements.txt
+python2 DarkFB.py
+fi
+
+if [ $nomor = 21 ] || [ $nomor = 21 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/TheMagizz/DarkVip
+cd DarkVip
+pip2 install -r requirements.txt
+python2 darkvip.py
+fi
